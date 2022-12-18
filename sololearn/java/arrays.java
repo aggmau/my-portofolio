bb2f671cdd80069606493302936bcb9ef4658703
aggmau <69064955+aggmau@users.noreply.github.com>
2022-12-18 22:10:57 +0700

    Create arrays.java

---
+++ sololearn/java/arrays.java
@@ -0,0 +1,14 @@
+/*
+Arrays :
+Your calendar program should output all the days of week, but it has errors.
+Change the code so that the program prints the days.
+*/
+
+public class Main {
+   public static void main(String[] args) {
+       String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+       for (int i = 0; i < 7; i++) {
+           System.out.println(days[i]);
+       }
+   }
+}
