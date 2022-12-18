12e4e240d192d06ca36eb5ff4901bd4ec2328045
aggmau <69064955+aggmau@users.noreply.github.com>
2022-12-18 17:21:59 +0700

    Create comment-or-uncomment.java

---
+++ sololearn/java/comment-or-uncomment.java
@@ -0,0 +1,14 @@
+/*
+Java Comments :
+You are given lines of commented code. Uncomment the required lines in order to have a "Java is awesome" output.
+*/
+
+public class Main {
+
+   public static void main(String[] args) {
+       //uncomment necessary line(s)
+       //System.out.println("Java is lame");
+       System.out.println("Java is awesome");
+       //System.out.println("Please uncomment me!!! ^^");
+   }
+}
