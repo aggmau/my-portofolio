b24db27473912d1f06ae26f92fd037831d9c787a
aggmau <69064955+aggmau@users.noreply.github.com>
2022-12-18 17:23:07 +0700

    Create once-in-a-bar.java

---
+++ sololearn/java/once-in-a-bar.java
@@ -0,0 +1,18 @@
+/*
+Primitive Operators
+A bartender sold 64 bottles of beer and 23 bottles of whisky. You need to calculate how many total bottles are sold.
+Task
+Calculate and output the total number of sold bottles.
+*/
+
+public class Program
+{
+    public static void main(String[] args) {
+	int beer  = 64;
+        int whisky = 23;
+       //calculate the sum and output it
+       int sum = beer + whisky;
+       System.out.println(sum);
+        
+	}
+}
