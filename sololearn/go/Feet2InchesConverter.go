a5a77691044113e4f6212a1dda7d00a30e59552c
Agung M <69064955+aggmau@users.noreply.github.com>
2023-01-04 22:50:41 +0700

    Create Feet2InchesConverter.go

---
+++ sololearn/go/Feet2InchesConverter.go
@@ -0,0 +1,23 @@
+/*Feet to Inches Converter
+
+
+You need to create a program that converts feet to inches and outputs the resulting value.
+The feet value is provided as the input to the program.
+1 foot = 12 inches.
+
+Sample Input:
+5
+
+Sample Output:
+60
+*/
+
+package main
+import "fmt"
+
+func main() {
+    //your code goes here
+    var x int
+    fmt.Scanln(&x)
+    fmt.Println(x*12)
+}
