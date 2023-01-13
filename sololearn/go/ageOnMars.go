f906e55d366f817abe457d7583a6508185435fdf
Agung M <69064955+aggmau@users.noreply.github.com>
2023-01-13 19:49:47 +0700

    Create ageOnMars.go

---
+++ sololearn/go/ageOnMars.go
@@ -0,0 +1,31 @@
+/*
+Age on Mars
+
+
+How old would you be on Mars?
+A year on Earth has 365 days, while a year on Mars has 687 days.
+
+Create a program that takes your age in Earth years as input, and outputs the corresponding age on Mars.
+
+The given program takes an integer as input and passes it to the mars_age() function as argument.
+Complete the function to calculate and return the corresponding Mars age.
+
+Sample Input:
+42
+
+Sample Output:
+22
+Note that the output is an integer.
+Calculate the Earth days corresponding to the given age and divide it by the number of days in a year on Mars to get the result.
+*/
+
+package main
+import "fmt"
+
+func main() {
+    var age int
+    fmt.Scanln(&age)
+
+    mars := mars_age(age)
+    fmt.Println(mars)
+}
